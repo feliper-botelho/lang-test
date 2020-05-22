69a912f6a9afedf5f0762ddcb140191184212a76
Felipe Botelho <felipebotelho18@gmail.com>
2020-05-22 11:24:37 -0300

    initial

---
+++ pt.js
@@ -0,0 +1,99 @@
+const pt = {
+  translation: {
+    header: {
+      ecommerce: "E-commerce",
+      precos: "Preços",
+      produtos: "Produtos",
+      splitPagamento: "Split de Pagamento",
+      boletos: "Boletos",
+      maquininhas: "Maquininhas",
+      devs: "Desenvolvedores",
+      ajuda: "Ajuda",
+      inicarSessao: "Inicar Sessão",
+    },
+    comunicado: {
+      comunicadoImportante: "Comunicado importante:",
+      comunicado:
+        "Durante as festas de fim de ano, somente nosso time de suporte e sustenção estarão disponíveis para casos de emergência. Boas festas e feliz 2021!",
+    },
+    home: {
+      receba: {
+        title: "Receba os seus pagamentos de modo fácil e totalmente seguro",
+        text:
+          "Ferramentas para rodar seu negócio mesmo quando a atividade não é tão comum. Crie a sua solução de pagamento personalizada de acordo com seu ramo de atividade.",
+        criarConta: "CRIAR CONTA",
+        falarTime: "FALAR COM TIME COMERCIAL",
+      },
+      pessoas: {
+        title: "Mais de 2 milhões de pessoas jápagaram usando AZPAY",
+        text:
+          "Nosso compromisso é aceitar diversos meios de pagamentos de diferentes adquirentes sem dor, de forma segura, simples e objetiva.",
+        sla: "Nosso SLA em quase 1 década se manteve a 99,999% mensalmente",
+        transacoes: "Transações efetuadas",
+        processados: "Reais processados",
+        cartoes: "Cartões de créditos",
+        desde: "* desde 2013",
+      },
+      vendasInternet: {
+        title: "Venda na internet do jeito que você quer:",
+        checkout: "Checkout transparente",
+        checkoutText:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+        checkoutButton: "TESTE AGORA",
+        api: "API de pagamentos",
+        apiText:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+        apiButton: "CONHEÇA MAIS",
+        integracoes: "Integrações",
+        integracoesText:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+        integracoesButton: "CONHEÇA MAIS",
+        recursos: "VEJA OUTROS RECURSOS",
+      },
+      maquininhas: {
+        title: "Venda presencial",
+        subtitle: "Maquininhas para o seu bolso",
+        text:
+          "Temos diversos modelos e preços que cabem no seu bolso. Ofereça pagamentos com todas as bandeiras e receba em até um dia útil de forma simples e rápida mesmo você sendo desbancarizado ou com restrições.",
+        button: "SOLICITE A SUA MAQUININHA",
+      },
+      cartoes: {
+        title: "Cartão pré-pago com saldo dassuas vendas online epresenciais",
+        text:
+          "Todas as vendas pela internet ou maquininhas são creditados no seu cartão pré-pago AZPAY. É uma forma simples e eficaz para você que precisa de agilidade no fluxo de caixa do seu negócio.",
+        button: "CONHEÇA MAIS",
+      },
+      produtosPensados: {
+        title: "Produtos pensados para o seu negócio!",
+        contaDigital: {
+          title: "Conta digital",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+          button: "Entenda como funciona >>>",
+        },
+        recorrencia: {
+          title: "Recorrência",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+          button: "Entenda como funciona >>>",
+        },
+        split: {
+          title: "Split de pagamentos",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
+          button: "Entenda como funciona >>>",
+        },
+        boleto: "Boleto registrado",
+        gateway: "Gateway de pagamento",
+        tokenizacao: "Tokenização de cartões",
+        transferencias: "Transferências / Débito",
+        cash: "Cash in - Cash out",
+        whitelabel: "Whitelabel / Marketplace",
+        sdk: "SDK's de integração",
+        conciliacao: "Conciliação bancária",
+        plugins: "Plugins",
+        vejaLista: "Veja a lista completa",
+      },
+    },
+  },
+};
