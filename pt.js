4200bedbca07d852836c50e93aae08a3201eafd8
Felipe Botelho <felipebotelho18@gmail.com>
2020-05-25 11:10:12 -0300

    n

--- pt.js
+++ pt.js
@@ -11,11 +11,13 @@ const pt = {
       ajuda: "Ajuda",
       inicarSessao: "Inicar Sessão",
     },
+
     comunicado: {
       comunicadoImportante: "Comunicado importante:",
       comunicado:
         "Durante as festas de fim de ano, somente nosso time de suporte e sustenção estarão disponíveis para casos de emergência. Boas festas e feliz 2021!",
     },
+
     home: {
       receba: {
         title: "Receba os seus pagamentos de modo fácil e totalmente seguro",
@@ -93,6 +95,167 @@ const pt = {
         conciliacao: "Conciliação bancária",
         plugins: "Plugins",
         vejaLista: "Veja a lista completa",
+      },
+      clientes: {
+        title: "Eles acreditam em nós,voce também deveria",
+      },
+      helpDesk: {
+        text:
+          "Se você quer saber mais sobre os produtos AZPAY ou serviços, nós estamos aqui com as respostas. Fale com nosso time entre as 9h e 18h de segunda a sexta.",
+        chat: "Chat online",
+        helpDesk: "Ou via Help-Desk",
+      },
+      impactar: {
+        title: "Impactar positivamente as duas vendas é a nossa missão!",
+        text:
+          "Desde fintechs ao varejo podemos ajudar a aumentar a conversão do seus pagamentos de forma efetiva. Crie a sua conta conosco e teste sem compromissos e se ainda tiver dúvidas fale com nosso time!",
+        criarBtn: "CRIE SUA CONTA GRATUITAMENTE",
+        falar: "Falar com time comercial",
+      },
+    },
+    footer: {
+      atendimento: "Atendimento Geral",
+      suporte: "Suporte técnico",
+      vendas: "Vendas / Comercial",
+      financeiro: "Financeiro / Contabilidade",
+      produtos: "Produtos / Serviços",
+      produtosItems: {
+        checkout: "Checkout transparente",
+        api: "API de pagamentos",
+        maquininhas: "Maquininhas / POS",
+        split: "Split de pagamento",
+        integracoes: "Integrações",
+        plugins: "Plugins",
+        gateway: "Gateway de pagamento",
+        credenciamento: "Credenciamento",
+        recorrencia: "Recorrência",
+      },
+      base: "Base de conhecimento",
+      baseItems: {
+        blog: "Blog",
+        central: "Central do desenvolvedor",
+        faq: "FAQ",
+        suporte: "Suporte / Help Desk / Abertura de chamado",
+        status: "Status",
+        precos: "Preços",
+      },
+    },
+    empresa: {
+      title:
+        "Nosso objetivo é tornar os seus recebimentos mais acessíveis, seguros e transparentes sem a tradicional dificuldade de integração e configuração",
+      text1:
+        "Começamos a inovar o mercado de pagamentos online em 2013. Desde o primeiro dia nós queríamos fazer os sites de ecommerces venderem mais com o custo menor por transação e uma única transação com menos dificuldade do que as soluções existentes na época.",
+      title2:
+        "Convencer empresas a migrarem a soluções de pagamentos online não foi fácil.",
+      text2:
+        "Mas nós temos uma grande vantagem: economia de taxas com facilidade para o dono do negócio, o time financeiro e os desenvolvedores. Não queriamos facilitar apenas um deles, isso seria muito fácil. Usando qualquer uma das nossas soluções, o seu site aproveita tudo de melhor da solução de pagamentos mais inteligente da internet. Tudo que criamos foi pensando nas necessidades do seu usuário, porque assim o seu negócio prospera e para nós, missão cumprida.",
+    },
+
+    precos: {
+      funcionam: {
+        title: "Como funcionam os preços",
+        text:
+          "O pagamento é conforme o crescimento do seu volume de transações. Isso permite que você se adapte facilmente às necessidades de mercado sem sobrecarregar orçamentos economizando com custos de implementação de software e integração com adquirentes e instituições financeiras.",
+        taxa: {
+          title: "TAXA MDR: CARTÕES",
+          credito: "CRÉDITO",
+          debito: "DÉBITO",
+          rodape:
+            "As taxas são por MCC, podendo haver ajustes dependendo do ramo de atividade de cada lojista.",
+        },
+      },
+      gateway: {
+        title: "GATEWAY DE PAGAMENTO",
+        aPartir: "A partir de:",
+        por: "POR TRANSAÇÃO VÁLIDA",
+        rodape: "Cálculo baseado em quantidade de transações mensais",
+      },
+      boleto: {
+        title: "BOLETO BANCÁRIO",
+        aPartir: "A partir de:",
+        por: "POR BOLETO COMPENSADO",
+        rodape: "Cálculo baseado em quantidade de transações mensais",
+      },
+      termos:
+        "Termos e condições especiais para parceiros e projetos com grandes volumes",
+      taxaMdr: {
+        title: "Taxa MDR: Cartões",
+        text:
+          "A taxa MDR (sigla em inglês para Merchant Discount Rate) ou conhecida como “Taxa de Desconto do Lojista”, trata-se de uma taxa percentual, cobrada pela processadora de cartão sobre os lojistas, em cada transação realizada na máquina de cartão, seja ela de débito ou crédito.",
+      },
+      transacoes: {
+        title: "Transações válidas",
+        text:
+          "Transações válidas são as transações que foram aprovadas, autorizadas, capturadas e canceladas. Atualmente o AZPAY cobra somente sobre as transações válidas. O que significa que você só paga por transações que geraram receitas para vocês. Transações negadas ou não autorizadas não são cobradas.",
+      },
+      boletosCompensados: {
+        title: "Boletos compensados",
+        text:
+          "Boletos compensados, são boletos que foram pagos pelo consumidor ao ser emitido pelo AZPAY. Atualmente cobramos apenas por boletos que foram pagos, ou seja, que geraram receita para você. Boletos que foram emitidos e não foram pagos ou venceram, não são cobrados.",
+      },
+      gatewayPagamento: {
+        title: "Gateway de pagamento",
+        text:
+          "Podemos transacionar seus pagamentos em praticamente todos os adquirentes do Brasil, caso você queria aumentar as suas opções de provedores sem perder as taxas exististentes e além disso ter um backup para seu sistema de pagamento, o nosso gateway é uma excelente alternativa. Cobramos por faixa de preços x volume mensal. Quanto mais você transacionar, menos você paga por transação.",
+      },
+      perguntasFrequentes: "Perguntas frequentes",
+      asTaxas: {
+        title: "As taxas são para online e presencial?",
+        text:
+          "Sim, as taxas uma vez acordadas são mantidas para ambos os cenários. Isso é um diferencial no mercado, em geral você encontra taxas para os diferentes cenários.",
+      },
+      prazo: {
+        title: "Qual o prazo de recebimento do dinheiro?",
+        text:
+          "O prazo padrão para transações de cartões de crédito é de d+30, ou seja você recebe todo dia as transações efetuadas sempre nos 30 dias anteriores diretamente na sua conta bancária. Para cartões de débito e boleto é em até um dia útil.",
+      },
+      antecipacao: {
+        title:
+          "qual a taxa e prazos de antecipação do dinheiro para cartões de crédito?",
+        text:
+          "Vendas com cartões de crédito podemos fazer a antecipação das parcelas com a taxa de 1.9%. Vendas presenciais tem esse recurso habilitado imediatamente, para vendas online necessita uma avaliação do nosso time de risco para a liberação.",
+      },
+    },
+    produtos: {
+      title: "Cobertura de soluções para atender o seu negócio!",
+      tabela: {
+        pagOnline: "Pagamento online",
+        pagPresen: "Pagamento presencial",
+        financeiro: "Finaceiro",
+        gestao: "Gestão",
+        token: "Tokenização",
+        cobranca: "Cobrança com Boleto",
+        recorrencia: "Recorrência / Assinaturas",
+        split: "Split de pagamentos",
+        gateway: "Gateway de pagamento",
+        api: "API de pagamento",
+        checkout: "Checkout Transparente",
+        notificacoes: "Notificações de pagamento",
+        plataforma: "Plataforma de e-commerce",
+        maquininhas: "Maquininhas / POS",
+        bobinas: "Bobinas / Suprimentos",
+        sdk: "SDKs iOS / Android",
+        personalizacao: "Personalização / Whitelabel",
+        bandeiras: "Aceitamos as principais bandeiras",
+        conta: "Conta digital integrada",
+        antecipacao: "Antecipação de recebíveis",
+        transferencia: "Transferência entra contas AZPAY",
+        cash: "Cash in / Cash out",
+        disputas: "Gestão de disputas e chargebacks",
+        painel: "Painel centralizado",
+        conciliacao: "Conciliação financeira",
+        cartao: "Cartão Pré-Pago",
+        relatorios: "Relatórios completos",
+        cobranca: "Cobrança manual",
+        multi: "Multi-contas",
+        analise: "Análise de riscos e fraude",
+        politicas: "Políticas de recebimento",
+        documentacao: "Documentação técnica",
+      },
+      cards: {
+        bandeiras: "Bandeiras",
+        bandeirasText:
+          "Suportamos pagamentos através de cartões de crédito das bandeiras Visa, Master, Amex, ELO, Diners, Discover, Aura, JCB, Hipercard. Suporte a realização de transações de pré-autorização de venda com captura total ou parcial do valor.",
       },
     },
   },
